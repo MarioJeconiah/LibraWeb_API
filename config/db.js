daf7c8df573905f1f50aabc98e1ac70caeec1bb6
MarioJeconiah <mariopurba40@gmail.com>
2025-12-07 19:06:16 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  password: process.env.DB_PASS || "",
+  password: process.env.DB_PASS,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
